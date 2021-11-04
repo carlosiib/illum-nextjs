91ccc49560f39b4b99bb94760a9e5e29add83ca9
carlosiib <ntic.carlos@gmail.com>
2021-11-03 19:24:24 -0700

    FRAMER path svgs animation

--- components/StartAcademyDesktop/index.js
+++ components/StartAcademyDesktop/index.js
@@ -1,14 +1,37 @@
 import React, { useState, useEffect } from 'react'
-import { motion } from "framer-motion"
+import { motion, useAnimation } from "framer-motion"
 import styles from '../../styles/StartAcademy.module.css'
 
 const StartAcademyDesktop = () => {
   const [currentContent, setCurrentContent] = useState("Instructors")
   const [isSelected, setIsSelected] = useState(false)
 
+  // Intersection observer Hook for animations
+  const animation = useAnimation()
+
   useEffect(() => {
     setIsSelected(true)
   }, [])
+
+  useEffect(() => {
+    if (true) {
+      animation.start({
+        opacity: 1,
+        pathLength: 1,
+        transition: {
+          duration: 2,
+          ease: "easeInOut"
+        }
+      })
+    }
+    // if (true) {
+    //   animation.start({
+    //     opacity: 0,
+    //     pathLength: 0,
+    //   })
+    // }
+
+  }, [animation])
 
 
   function handlePathContent(e) {
@@ -42,56 +65,64 @@ const StartAcademyDesktop = () => {
       </div>
 
       <div className={styles.svgsRow}>
-        <div>
-          <svg
+        <motion.div>
+          <motion.svg
             xmlns="http://www.w3.org/2000/svg"
             width="1040"
             height="100"
             viewBox="0 0 800 97"
             fill="none"
+            initial="hidden"
+            animate="visible"
           >
-            <path
+            <motion.path
               d="M581 0V39.2718C581 42.0332 578.761 44.2718 576 44.2718H548.416H5.99999C3.23857 44.2718 1 46.5104 1 49.2718V97"
               stroke={isSelected && currentContent === "Content Managers" ? "#52b56b" : "#3f85ca"}
               strokeWidth="3"
+              animate={animation}
             />
-          </svg>
-        </div>
+          </motion.svg>
+        </motion.div>
 
 
-        <div>
-          <svg
+        <motion.div>
+          <motion.svg
             xmlns="http://www.w3.org/2000/svg"
             width="1058"
             height="100"
             viewBox="0 0 5 100"
             fill="none"
-
+            initial="hidden"
+            animate="visible"
           >
-            <path
+            <motion.path
               d="M225 0V63.1592C225 65.9206 222.761 68.1592 220 68.1592H5.99999C3.23857 68.1592 1 70.3978 1 73.1592V100"
               stroke={isSelected && currentContent === "Learners" ? "#52b56b" : "#3f85ca"}
               strokeWidth="3"
+              animate={animation}
             />
-          </svg>
-        </div>
+          </motion.svg>
+        </motion.div>
 
-        <div>
-          <svg
+        <motion.div>
+          <motion.svg
             xmlns="http://www.w3.org/2000/svg"
             width="1060"
             height="113"
             viewBox="-380 10 300 99"
             fill="none"
+            initial="hidden"
+            animate="visible"
 
           >
-            <path
+            <motion.path
               d="M1 0V62.1608C1 64.9222 3.23858 67.1608 6 67.1608H106C108.761 67.1608 111 69.3994 111 72.1608V99"
               stroke={isSelected && currentContent === "Instructors" ? "#52b56b" : "#3f85ca"}
               strokeWidth="3"
+              animate={animation}
             />
-          </svg>
-        </div>
+          </motion.svg>
+        </motion.div>
       </div>
 
       {/* PATH BUTTONS */}
@@ -99,6 +130,7 @@ const StartAcademyDesktop = () => {
         <button
           onClick={handlePathContent}
           data-content-for="Content Managers"
+          className={isSelected && currentContent === "Content Managers" ? "path-main-btn" : "path-sec-btn"}
         >
           For Content Managers
           <img
@@ -111,7 +143,8 @@ const StartAcademyDesktop = () => {
         </button>
         <button
           onClick={handlePathContent}
-          data-content-for="Learners">
+          data-content-for="Learners"
+          className={isSelected && currentContent === "Learners" ? "path-main-btn" : "path-sec-btn"}>
           For Learners
           <img
             src="/svg/btn-book.svg"
@@ -123,7 +156,9 @@ const StartAcademyDesktop = () => {
         </button>
         <button
           onClick={handlePathContent}
-          data-content-for="Instructors">
+          data-content-for="Instructors"
+          className={isSelected && currentContent === "Instructors" ? "path-main-btn" : "path-sec-btn"}
+        >
           For Instructors
           <img
             src="/svg/btn-instructor.svg"
