c76dccc82ee7cc4653b7f44ab5bfd8e19a03d02b
carlosiib <ntic.carlos@gmail.com>
2021-11-09 16:19:20 -0700

    404 page

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
 
 const PageNotFound = () => {
   return (
-    <div>
-      <h1>404 | Page not found</h1>
-    </div>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Notebooks as a service for LMS systems" />
+        <link rel="icon" href="/icon.png" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <title>ILLUMIDESK</title>
+      </Head>
+
+      <div className="notFoundPageContainer">
+        <div>
+          <h1>404</h1>
+          <h2>Not found</h2>
+          <p>The page you requested doesn<span>&apos;</span>t exist.</p>
+          <Link href="/">
+            <a>Go home</a>
+          </Link>
+        </div>
+
+      </div>
+    </>
   )
 }
 
